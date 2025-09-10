10e36318432f5c21c5f0477dd4c43e71c4ddc460
Safwan Amin <safwan.amin@g.bracu.ac.bd>
2025-09-10 14:12:10 +0600

    Add Octokit integration for user authentication and viewer data retrieval

---
+++ src/server/app/octokit/index.ts
@@ -0,0 +1,41 @@
+import { Session } from "@auth/qwik";
+import { Octokit } from "octokit";
+import { getAccessTokenByUserId } from "~/server/db";
+
+
+export const getOctokit = async (session: Session) => {
+    const userId = session?.user?.id;
+    if (!userId) {
+        throw new Error("No user ID found in session");
+    }
+    const accessToken = await getAccessTokenByUserId(userId);
+    if (!accessToken) {
+        throw new Error("No access token found for user");
+    }
+    return new Octokit({auth: accessToken});
+};
+
+export const getViewerData = async (octokit: Octokit) => {
+    const data = await octokit.graphql<{
+        viewer: {
+          login: string;
+          name: string;
+          avatarUrl: string;
+          url: string;
+          email: string;
+        };
+      }>(
+        `#graphql
+        query {
+          viewer {
+            login
+            name
+            avatarUrl
+            url
+            email
+          }
+        }
+        `,
+      );
+    return data.viewer;
+}
